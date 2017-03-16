83b1e1725b96fabd1ce7a0e3be5092b74ad073eb
Yun Li <anyayunli@gmail.com>
2017-03-16 11:33:57 -0700

    reset hasVoted

--- voting-react/test/reducer_spec.js
+++ voting-react/test/reducer_spec.js
@@ -46,4 +46,66 @@ describe('reducer', () => {
     }))
   });
 
+  it('handles VOTE by setting hasVoted', () => {
+    const state = fromJS({
+      vote: {
+        pair: ['A', 'B'],
+        tally: { 'A': 1 }
+      }
+    });
+    const action = { type: 'VOTE', entry: 'A' };
+    const nextState = reducer(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['A', 'B'],
+        tally: { 'A': 1 }
+      },
+      hasVoted: 'A'
+    }));
+  })
+
+  it('handles VOTE by not setting hasVoted for invalid entry', () => {
+    const state = fromJS({
+      vote: {
+        pair: ['A', 'B'],
+        tally: { 'A': 1 }
+      }
+    });
+    const action = { type: 'VOTE', entry: 'C' };
+    const nextState = reducer(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['A', 'B'],
+        tally: { 'A': 1 }
+      }
+    }));
+  })
+
+  it('removes hasVoted on SET_STATE if pair changes', () => {
+    const state = fromJS({
+      vote: {
+        pair: ['C', 'B'],
+        tally: { 'C': 1 }
+      },
+      hasVoted: 'C'
+    });
+    const action = {
+      type: 'SET_STATE',
+      state: {
+        vote: {
+          pair: ['A', 'B']
+        }
+      }
+    };
+    const nextState = reducer(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['A', 'B'],
+      }
+    }));
+  })
+
 });
